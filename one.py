fcd70ab274c0faad001d7ff110785eebe21b655b
nicktamburro <nicholastamburro@gmail.com>
2019-03-13 22:40:10 -0700

    diving back into python after some years away

---
+++ one.py
@@ -0,0 +1,32 @@
+print("Welcome back to your old friend Python.")
+
+#remember no semi-colons, and indentation actually matters!
+
+if 5 > 2
+    print("You bet it is, buddy.")
+
+"""and remember this is for
+multiline comments"""
+
+#you don't have to specify a variable type, just name it and define it
+band = "Mudhoney"
+previousBand = "Green River"
+print(band)
+print(previousBand)
+
+#var types can change, no problem
+album = 2112
+print(album)
+album = "Permanent Waves"
+print(album)
+
+#concat is same as js
+print("He was in " + previousBand + " before he was in " + band + "."
+
+# + will also add, if you give it numbers
+x = 100
+y = 1
+print(x + y)
+
+#but, thankfully, you'll get an error if you do:
+print(x + album)
