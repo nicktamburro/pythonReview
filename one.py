869b0442d14ec30293b86b111aa84ed555edf40c
nicktamburro <nicholastamburro@gmail.com>
2019-03-15 21:44:30 -0700

    operators

--- one.py
+++ one.py
@@ -92,6 +92,48 @@ these are new:
 ** - exponentation
 // floor division (like math.floor)
 
+give these all another look 
+=	x = 5	x = 5	
++=	x += 3	x = x + 3	
+-=	x -= 3	x = x - 3	
+*=	x *= 3	x = x * 3	
+/=	x /= 3	x = x / 3	
+%=	x %= 3	x = x % 3	
+//=	x //= 3	x = x // 3	
+**=	x **= 3	x = x ** 3	
+&=	x &= 3	x = x & 3	
+|=	x |= 3	x = x | 3	
+^=	x ^= 3	x = x ^ 3	
+>>=	x >>= 3	x = x >> 3	
+<<=	x <<= 3	x = x << 3"""
+
+#the comparisons are mostly the same
+# ==, !=, >, <, >=, <=
+
+#we can also use the words
+#logical operators
+and
+or
+not
+#identity operators
+is
+is not
+#membership operators
+in
+not in
+
+"""and spend a little more time with these
+Bitwise Operators
+& 	AND	Sets each bit to 1 if both bits are 1
+|	OR	Sets each bit to 1 if one of two bits is 1
+ ^	XOR	Sets each bit to 1 if only one of two bits is 1
+~ 	NOT	Inverts all the bits
+<<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off
+>>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+"""
+
+
+
 
 
 
